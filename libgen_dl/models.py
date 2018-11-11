70b0c51b778fbdfd4457422b4ad0846dc7d9d052
Tyler Melton <tyler@tylerweb.net>
2018-11-11 01:12:11 -0500

    Added rough mockup

---
+++ libgen_dl/models.py
@@ -0,0 +1,55 @@
+"""
+This file holds all of the data models and associated classes for
+libgen_dl.
+
+Author:     Tyler Melton
+Created:    11/06/2018
+Modified:   11/11/2018
+"""
+
+from queue import Queue
+
+
+class Book():
+    """
+    Book() holds all the properties needed to describe a book
+    from libgen.io
+    """
+
+    def __init__(self):
+        self.md5 = ""
+        self.id = ""
+        self.title = ""
+        self.authors = []
+        self.publisher = ""
+        self.year = ""
+        self.extension = ""
+
+    def __str__(self):
+        s = f"({self.id}) =>"
+        s = f"{s} Title: {self.title}"
+        s = f"{s} (Year: {self.year})"
+        s = f"{s} (EXT: {self.extension})"
+
+        return s
+
+
+class ResultSet:
+    """
+    Holds a set of search results from libgen.io, including
+    pagination information
+    """
+
+    def __init__(self):
+        
+
+
+class DownloadQueue(Queue):
+    """
+    A queue for holding a list of books for download.
+
+    *** Currently, this is just a wrapper for python's Queue ***
+    """
+
+    def __init__(self):
+        super().__init__()
